0700979deaa24dc360fded6830e48a1fbabeaf69
Goncalocosta2922 <2023109174@ufp.edu.pt>
2024-05-10 19:58:27 +0100

    login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,32 @@
 import React from 'react'
 import '../styles/Login.css'
+import { FaUser } from "react-icons/fa";
+import { RiLockPasswordFill } from "react-icons/ri";
+
 
 const   Login = () => {
   return (
-    <div> 
-      <hi>Login</hi>
-      <input type="Text" />
-      <input type="Password" />
-      <button>Submit</button>
+    <div className='fullbox'>
+      <div className='Login'>
+        <h1>Login</h1>
+        <div className='username-input'>
+         <input type='Text' placeholder='Username' />
+         <FaUser className='icons'/>
+        </div>
+      
+        <div className='password-input'>
+         <input type='Password' placeholder='Password' />
+         <RiLockPasswordFill className='icons'/>
+        </div>
 
+       <a href='#'>Forgot Password?</a>
+      <button>Iniciar Sessão</button> 
+      </div>
+      <div className='Register'>
+        <h1>Ainda não tem conta?</h1>
+        <p>Se ainda não tem conta clique aqui para se registar</p>
+        <button>Registar-se<a href='#'></a></button>
+      </div>
   </div>
   )
 }
