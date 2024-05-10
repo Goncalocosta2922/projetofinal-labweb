2539b65aa6ca0b28c817cc3da975828956269b03
Goncalocosta2922 <2023109174@ufp.edu.pt>
2024-05-10 16:15:32 +0100

    footrt

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
 import '../styles/footer.css'
 
 function Footer() {
 return (
     <div className='footer'>
-        <p> Em caso de duvidas contacte-nos em marketplace@gmail.com ou 961228653 </p>
-        <InstagramIcon/>
+        <div className='social'>
+            <FacebookIcon/> <InstagramIcon/> <TwitterIcon/>
+        </div>
+        <p>Em caso de duvidas contacte-nos em marketplace@gmail.com ou 961228653</p>
     </div>
+
+        
 )
 }
 
