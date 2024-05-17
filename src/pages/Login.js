1cd8c61f447146ba8ccd4e8d2b499b3727f9e0d8
Andre2312 <2023109163@ufp.edu.pt>
2024-05-17 18:16:21 +0100

    aaa

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,31 +3,37 @@ import '../styles/Login.css'
 import { FaUser } from "react-icons/fa";
 import { RiLockPasswordFill } from "react-icons/ri";
 
+const Login = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Lógico de login
+  }
 
-const   Login = () => {
   return (
     <div className='fullbox'>
       <div className='Login'>
         <h1>Login</h1>
-        <div className='username-input'>
-          <input type='Text' placeholder='Username' />
-          <FaUser className='icons'/>
-        </div>
-      
-        <div className='password-input'>
-          <input type='Password' placeholder='Password' />
-          <RiLockPasswordFill className='icons'/>
-        </div>
+        <form>
+          <div className='username-input'>
+            <input type='text' placeholder='Username' required />
+            <FaUser className='icons'/>
+          </div>
+        
+          <div className='password-input'>
+            <input type='password' placeholder='Password' required />
+            <RiLockPasswordFill className='icons'/>
+          </div>
 
-      <a href='#'>Forgot Password?</a>
-      <button>Iniciar Sessão</button> 
+          <a href='#'>Forgot Password?</a>
+          <button type='submit'>Iniciar Sessão</button> 
+        </form>
       </div>
       <div className='Register'>
         <h1>Ainda não tem conta?</h1>
         <p>Se ainda não tem conta clique aqui para se registar</p>
         <button>Registar-se<a href='#'></a></button>
       </div>
-  </div>
+    </div>
   )
 }
 
